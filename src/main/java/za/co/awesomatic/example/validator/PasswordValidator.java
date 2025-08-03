5252f68eaaa7621c721032cd2c0a26c5263b68fb
donald <donald@awesomatic.co.za>
2025-08-03 08:55:19 +0200

    pre initialize rules on application start up

--- src/main/java/za/co/awesomatic/example/validator/PasswordValidator.java
+++ src/main/java/za/co/awesomatic/example/validator/PasswordValidator.java
@@ -1,6 +1,9 @@
 package za.co.awesomatic.example.validator;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.expression.Expression;
 import org.springframework.expression.ExpressionParser;
 import org.springframework.expression.spel.standard.SpelExpressionParser;
@@ -18,17 +21,25 @@ import java.util.function.Function;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class PasswordValidator implements Function<PasswordValidationRequest, PasswordValidationResponse> {
+    private List<PasswordRule> rules;
     private final PasswordRuleRepository repository;
     private final ExpressionParser parser = new SpelExpressionParser();
     private final Map<String, Expression> expressionCache = new ConcurrentHashMap<>();
     private final StandardEvaluationContext context = new StandardEvaluationContext();
 
+    @EventListener(ApplicationReadyEvent.class)
+    public void initializeRules() {
+        this.rules = repository.findAll();
+        log.info("Password rules initialized.");
+    }
+
     @Override
     public PasswordValidationResponse apply(PasswordValidationRequest request) {
         context.setVariable("password", request.getPassword());
 
-        List<String> messages = repository.findAll().stream()
+        List<String> messages = rules.stream()
                 .filter(rule -> !isPasswordValid(rule))
                 .map(PasswordRule::getMessage)
                 .toList();
